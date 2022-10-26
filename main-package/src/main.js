d496efb67d4b5141ad4fde095cc4653bd19ff9a3
吴焯斌 <920658022@qq.com>
2022-10-26 17:29:46 +0800

    feat:ie9

--- main-package/src/main.js
+++ main-package/src/main.js
@@ -1,13 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import VueRouter from "vue-router";
-import router from "./router";
+import 'babel-polyfill'
+import 'regenerator-runtime/runtime';
 import 'whatwg-fetch';
 import 'custom-event-polyfill';
 import 'core-js/stable/promise';
 import 'core-js/stable/symbol';
 import 'core-js/stable/string/starts-with';
 import 'core-js/web/url';
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import router from "./router";
+
 // import { registerMicroApps, start } from "qiankun";
 // import { appList } from "./microApp";
 
