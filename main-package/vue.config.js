945068973a99a1efb472cfc0ffdcf3e53c2d007e
吴焯斌 <920658022@qq.com>
2022-10-31 11:50:47 +0800

    feat:fix

--- main-package/vue.config.js
+++ main-package/vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
   publicPath: "/",
   css: {
     requireModuleExtension: true,
-    loaderOptions: {
-      
-    },
+    loaderOptions: {},
   },
+  // 兼容ie9，sockjs相关代码es6转es5
+  transpileDependecies: ["sockjs-client", "sockjs"],
 };
